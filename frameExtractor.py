dcf1182217501db8c32f5dd591cac3c93351636e
Jundong Qiao <qiaojundong@gmail.com>
2017-09-20 12:27:10 -0400

    Update frameExtractor.py

--- frameExtractor.py
+++ frameExtractor.py
@@ -1,9 +1,9 @@
 import cv2
 import os
 import sys
+import time
 
-
-def frameExtractor(videofile):
+def frameExtractor(videofile, sample_frequency):
 	# Read video file
 	vidcap = cv2.VideoCapture(videofile)
 
@@ -48,8 +48,8 @@ def frameExtractor(videofile):
 
 		print 'Read a new frame: grabbed!'
 		count += 1
-
-		cv2.imwrite(os.path.join(data_dir, "%05d.png" % count), image)
+		if count%sample_frequency == 0:
+			cv2.imwrite(os.path.join(data_dir, "%05d.png" % count), image)
 
 
 #####################
@@ -58,5 +58,33 @@ def frameExtractor(videofile):
 if __name__ == "__main__":
 	print '~~~ Loading .MP4 file...'
 	videofile = sys.argv[1]
-	frameExtractor(videofile)
+	sample_frequency = sys.argv[2]
+	
+	vidcap = cv2.VideoCapture(videofile, sample_frequency)
+	
+	if vidcap.isOpened():
+		print "The imported file is readable."
+
+		t0 = time.time()
+		frameExtractor(videofile)
+		t1 = time.time()
+
+		t = t1 - t0
+		print "The process took %.2f mins." %(round(t/60., 2))
+
+	else:
+		print "Could not open the input file"
+
+		print "Converting MP4 file..."
+		os.system('ffmpeg -i %s -c:v copy %s_Converted.mp4' %(videofile, videofile[:-4]))
+		vidcap = cv2.VideoCapture('%s_Converted.MP4' % videofile[:-4])	
+
+		print 'Conversion Completed!'
+		videofile_converted = '%s_Converted.MP4' % videofile[:-4]
+		t0 = time.time()
+		frameExtractor(videofile_converted)
+		t1 = time.time()
+
+		t = t1 - t0
+		print "The process took %.2f mins." %(round(t/60., 2))
 	
