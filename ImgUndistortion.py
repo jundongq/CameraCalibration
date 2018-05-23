1a8a11a1c61470a635656d04ff995d48892da4ac
Jundong Qiao <qiaojundong@gmail.com>
2018-05-23 11:25:24 -0400

    Make it portable for other code

--- ImgUndistortion.py
+++ ImgUndistortion.py
@@ -19,39 +19,80 @@ import numpy as np
 import cv2
 import glob
 import os
+import sys
+import time
+import argparse
 
-print '-----Loading Calibration Data: cameraMatrix, distCoeffs.....'
+"""
+Usage: ImgUndistortion.py -cp yourCameraMatrixfile -f yourImageFolder
+"""
 
+def img_undistort(camera_calibration, dirname):
 
-calibrationData = np.load('calibration_data.npz')
+	print '-----Loading Calibration Data: cameraMatrix, distCoeffs.....'
+	calibrationData = np.load(camera_calibration)
+	distCoeffs   = calibrationData['distCoeffs']
+	cameraMatrix = calibrationData['cameraMatrix']
+	# close the file handle
+	calibrationData.close()
 
-distCoeffs   = calibrationData['distCoeffs']
-cameraMatrix = calibrationData['cameraMatrix']
+	# grab all images 
+	images = glob.glob(dirname + '/sampledFrames/*.png')
+	print len(images)
+	# loop through all the available images, to save object points and image points of the corners
+	print "-----Image undistortion in process...."
 
-# close the file handle
-calibrationData.close()
-
-# grab all images 
-images = glob.glob('ImgSamples/120/*.png')
-
-# loop through all the available images, to save object points and image points of the corners
-print "-----Image undistortion in process...."
-
-for i, fname in enumerate(images):
-	img = cv2.imread(fname)
-	# undistort the image
-	undistorted_img = cv2.undistort(img, cameraMatrix, distCoeffs, None)
-	'''The function transforms an image to compensate radial and tangential lens distortion. Those pixels in the destination image, for which there is no correspondent pixels in the source image, are filled with zeros (black color).'''
-	
 	# making a new directory to strore undistorted images
-	dirname = 'ImgSamples/Undistorted'
+	dirname = dirname + '/Undistorted'
 	if not os.path.exists(dirname):
 		os.mkdir(dirname)
-	
-	# save the undistorted image to the new diretory
-	cv2.imwrite(os.path.join(dirname,'Undistorted_%05d.png' %i), undistorted_img)
-	cv2.imshow('Undistorted Image %s' % fname, undistorted_img)
-	cv2.waitKey(500)
-cv2.destroyAllWindows()
 
-print '-----Undistortion Completed!'
+	for i, fname in enumerate(images):
+		img = cv2.imread(fname)
+		h,  w = img.shape[:2]
+		# print h, w
+		newcameramtx, roi=cv2.getOptimalNewCameraMatrix(cameraMatrix,distCoeffs,(w,h),1.,(w,h))
+		# print newcameramtx
+		# print roi
+		
+		# undistort the image
+		# undistorted_img = cv2.undistort(img, cameraMatrix, distCoeffs, None)
+		undistorted_img = cv2.undistort(img, cameraMatrix, distCoeffs, newcameramtx)
+		'''The function transforms an image to compensate radial and tangential lens distortion. Those pixels in the destination image, for which there is no correspondent pixels in the source image, are filled with zeros (black color).'''
+		
+		# crop the image
+		x,y,w,h = roi
+		if x!= 0:
+		
+			undistorted_img = undistorted_img[y:y+h, x:x+w]
+		
+		# save the undistorted image to the new diretory
+		cv2.imwrite(os.path.join(dirname,'Undistorted_%05d.png' %i), undistorted_img)
+		#cv2.imshow('Undistorted Image %s' % fname, undistorted_img)
+		#cv2.waitKey(500)
+	#cv2.destroyAllWindows()
+
+	print '-----Undistortion Completed!'
+
+#####################
+###### Running ######
+#####################
+if __name__ == "__main__":
+
+	print '~~~~ Grabbing img files...'
+	ap = argparse.ArgumentParser()
+	ap.add_augment('-cp', '--cam_paras', required=True, help='a .npz file containing camera calibration data')
+	ap.add_agument('-f', '--folder_name', required=True, help='a fold containing all .png files that need to be undistorted')
+	args = vars(ap.parse_args())
+	
+	cam_ = args['cam_paras']
+	dir_ = args['folder_name']
+
+	t0 = time.time()
+
+	img_undistort(cam_, dir_)
+
+	t1 = time.time()
+
+	t = t1 - t0
+	print "The process took %.2f mins." %(round(t/60., 2))
